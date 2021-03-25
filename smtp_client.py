6222b4ad1e8f0ddd0d088d083c7140ddcd26560c
Daniel Paiva <danielpaiva1993@gmail.com>
2021-03-25 19:29:31 -0300

    Envio de email quase implementado, precisa de mais testes

--- smtp_client.py
+++ smtp_client.py
@@ -1,8 +1,19 @@
 import sys
 from socket import *
 
+#inputs do usuario
+print("Digite o número da operação que se deseja fazer?")
+print("1 - Enviar Email")
+print("2 - Ler Emails")
+usrOp = int(input("Digite aqui o número da opção escolhida: "))
 
-portaServidor = 587
+if usrOp == 1:
+    remetente = input("Digite o email do remetente(mesmo nome do arquivo txt da caixa de entrada):")
+    destinatario = input("Digite o email do destinatario(mesmo nome do arquivo txt da caixa de entrada):")
+    corpoMsg = input("Digite a mensagem que deseja enviar:")
+
+
+portaServidor = 25
 host = 'localhost'
 
 try:
@@ -20,9 +31,45 @@ socketCliente.send(comando.encode())
 
 respostaHELO = socketCliente.recv(1024)
 
-if respostaHELO.decode() != 'OK - HELO recebido':
+if respostaHELO.decode() == '250 OK - HELO recebido':
+    print(respostaHELO.decode())
+else:
     print('Não houve resposta ao HELO')
     socketCliente.close()
     sys.exit()
+
+
+if usrOp == 1: # Enviar email
+    prontoParaEnvio = False
+    # Enviar o comando SMTP: MAIL FROM para o servidor
+    socketCliente.send('MAIL FROM:<' + remetente + '>'.encode())
+    respostaMF = socketCliente.recv(1024)
+
+    if respostaMF.decode() == '250 - MAIL FROM OK':
+        # Enviar o comando SMTP: RCPT TO para o servidor
+        socketCliente.send('RCPT TO:<' + destinatario + '>'.encode())
+        respostaRT = socketCliente.recv(1024)
+        if respostaRT.decode() == '250 - RCPT TO OK':
+            prontoParaEnvio = True
+        else:
+            print("Destinatario Invalido")
+            sys.exit()
+
+    else:
+        print("Remetente Invalido")
+        sys.exit()
+    
+    #Se o remetente e destinatarios forem validos, iniciar DATA
+    if prontoParaEnvio:
+        socketCliente.send('DATA'.encode())
+        respostaDATA = socketCliente.recv(1024)
+        respostaDATAdecoded = respostaDATA.decode()
+        if respostaDATAdecoded == '354 - Envie conteudo da mensagem':
+            socketCliente.send(corpoMsg.encode()) # TODO mensagem devera ser enviada aos poucos ate enviar "." para finalizar
+
+elif usrOp == 2: # Ler email
+    #codigo de leitura de email
+    print("op2")
 else:
-    print('Resposta ao HELO Recebida')
+    print("Operação inválida, digite apenas o número da operação")
+    sys.exit()
--- smtp_server.py
+++ smtp_server.py
@@ -2,6 +2,14 @@ import re
 import sys
 from socket import *
 
+def escreverMsg(dest, msg):
+    nomeCx = dest + ".txt"
+    cxDest = open(nomeCx, "a")
+    full = msg + '\n\n'
+    cxDest.write(full)
+    cxDest.close()
+    return
+
 # Arquivo deve ser passado como argumento de linha de comando como especificado
 if len(sys.argv) < 2:
     print("Para executar, digite no terminal: python script.py usuarios.txt")
@@ -31,7 +39,7 @@ usrs.close()
 
 # Criar o socket do servidor
 # 25, 465, 587 sao portas padrões para SMTPs, também pode usar uma porta arbitrada
-portaServidor = 587
+portaServidor = 25
 nomeServidor = ''
 socketServidor = socket(AF_INET, SOCK_STREAM)   # Declaracao default do socket
 socketServidor.bind((nomeServidor, portaServidor)) # faz o bind com a porta escolhida
@@ -42,7 +50,7 @@ while True:
     # Espera conexao com o cliente
     try:
         socketConexao, addr = socketServidor.accept()
-        print("Conexão do cliente aceita!")
+        print("220 OK - Conexão do cliente aceita!")
     except:
         print("Conexao Falhou")
         sys.exit()
@@ -52,10 +60,61 @@ while True:
         helo = socketConexao.recv(1024)
         
         if helo.decode() == 'HELO':
-            mensagem_servidor = 'OK - HELO recebido'
+            mensagem_servidor = '250 OK - HELO recebido'
+            print(mensagem_servidor)
             socketConexao.send(mensagem_servidor.encode())
     except:
         print('Erro no recebimento do HELO')
         sys.exit()
+    
+    prontoParaRecebimento = False
+
+    #Recebe comando do cliente para decidir o que fazer
+    comandoEMAIL = socketConexao.recv(1024)
+    comandoEMAILdecoded = comandoEMAIL.decode()
+
+    #Se receber um MAIL FROM o servidor sabe que é um envio de email
+    if comandoEMAILdecoded[:9] == 'MAIL FROM':
+        remetente = comandoEMAILdecoded[11:-1]
+        #checa se o remetente existe
+        if remetente in caixas:
+            socketConexao.send('250 - MAIL FROM OK'.encode())
+        else:
+            socketConexao.send('Remetente Invalido'.encode())
+        
+        #Recebe o RCPT TO do cliente
+        comandoEMAIL = socketConexao.recv(1024)
+        comandoEMAILdecoded = comandoEMAIL.decode()
+        
+        if comandoEMAILdecoded[:7] == 'RCPT TO':
+            destinatario = comandoEMAILdecoded[9:-1]
+            #checa se o destinatario existe
+            if destinatario in caixas:
+                socketConexao.send('250 - RCPT TO OK'.encode())
+                prontoParaRecebimento = True
+            else:
+                socketConexao.send('Destinatario Invalido'.encode())
+        else:
+            print("Aqui command unrecognized???1")
+    #Talvez um elif aqui para leitura de email
+    else:
+        ###Later
+        print("Aqui command unrecognized???2")
+    
+    if prontoParaRecebimento:
+        comandoDATA = socketConexao.recv(1024)
+        comandoDATAdecoded = comandoDATA.decode()
+        if comandoDATAdecoded == 'DATA':
+            socketConexao.send('354 - Envie conteudo da mensagem'.encode())
+            corpo = socketConexao.recv(1024)
+            corpoDecoded = corpo.decode()
+            escreverMsg(destinatario, corpoDecoded) # TODO servidor deve esperar um "." para parar de escrever
+
+        else:
+            print("Aqui command unrecognized???3")
+
+
+        
+
     
 
